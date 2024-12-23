78b67d116d0f0845d6f92f6001a899f2435782c5
Will Chen <07704.will.chen@ict888.net>
2024-12-23 17:33:23 +0800

    fix: syntax error

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ int main(int argc, char* argv[]) {
     const u_char* packet;
     std::unordered_map<std::string, int> ip_count_map;
     while (pcap_next_ex(handle, &header, &packet) >= 0) {
-        std::cout << "Timestamp: " << header.ts.tv_sec << "." << header.ts.tv_usec;
-        std::cout << " | Captured Length: " << header.caplen << " bytes" << std::endl;
-        std::cout << " | Original Length: " << header.len << " bytes" << std::endl;
+        std::cout << "Timestamp: " << header->ts.tv_sec << "." << header.ts.tv_usec;
+        std::cout << " | Captured Length: " << header->caplen << " bytes" << std::endl;
+        std::cout << " | Original Length: " << header->len << " bytes" << std::endl;
         std::cout << std::endl;
 
         struct ip* ip_header = (struct ip *)(packet + 14);
