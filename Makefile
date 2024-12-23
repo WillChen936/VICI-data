2582da308289856318ba4222ab5c87dbeca5adc6
Will Chen <will.chen936@gmail.com>
2024-12-24 00:14:46 +0800

    feat: finish 1st part

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 install:
 	g++ -o main main.cpp -lpcap
+
+uninstall:
+	rm main *.csv
--- main.cpp
+++ main.cpp
@@ -1,62 +1,75 @@
 #include <iostream>
+#include <fstream>
 #include <unordered_map>
-#include <netinet/ip.h> // IP header
-#include <netinet/udp.h> // UDP header
-#include <arpa/inet.h> // inet_ntoa
+
 #include <pcap.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h> 
+#include <arpa/inet.h>
 
-using namespace std;
 
-int main(int argc, char* argv[]) {
-    const char* filepath = "./data.pcap";
+#define ETHERNET_HEADER_LEN 14
+
+void GroupByIPPort(const char* filepath) {
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* handle = pcap_open_offline(filepath, errbuf);
     if (handle == nullptr) {
-        cerr << "Error opening file: " << errbuf << endl;
-        return 1;
+        std::cerr << "Error opening file: " << errbuf << std::endl;
+        return;
     }
 
     struct pcap_pkthdr* header;
     const u_char* packet;
-    std::unordered_map<std::string, int> ip_count_map;
+    std::unordered_map<std::string, int> ip_port_count;
     while (pcap_next_ex(handle, &header, &packet) >= 0) {
-        std::cout << "Timestamp: " << header->ts.tv_sec << "." << header->ts.tv_usec;
-        std::cout << " | Captured Length: " << header->caplen << " bytes";
-        std::cout << " | Original Length: " << header->len << " bytes" << std::endl;
-        std::cout << std::endl;
+        const struct ip* ip_header = (struct ip*)(packet + ETHERNET_HEADER_LEN);
+        auto ip_header_len = ip_header->ip_hl * 4;
 
-        struct ip* ip_header = (struct ip *)(packet + 14);
-        std::cout << std::hex << "packet + 13: 0x" << +*(packet + 13) << " | "
-              << "packet + 14: 0x" << +*(packet + 14) << " | "
-              << "packet + 15: 0x" << +*(packet + 15) << std::endl;
-        std::cout << "IP Header:" << std::endl;
-        std::cout << "  Version: " << (int)ip_header->ip_v << std::endl;
-        std::cout << "  Header Length: " << (int)ip_header->ip_hl * 4 << " bytes" << std::endl; // ip_hl 是 4 位，表示頭部長度（單位：4 字節）
-        std::cout << "  Type of Service: " << (int)ip_header->ip_tos << std::endl;
-        std::cout << "  Total Length: " << ntohs(ip_header->ip_len) << " bytes" << std::endl; // ntohs 轉換為主機字節序
-        std::cout << "  Identification: " << ntohs(ip_header->ip_id) << std::endl;
-        std::cout << "  Fragment Offset: " << ntohs(ip_header->ip_off) << std::endl;
-        std::cout << "  Time to Live: " << (int)ip_header->ip_ttl << std::endl;
-        std::cout << "  Protocol: " << (int)ip_header->ip_p << std::endl;
-        std::cout << "  Header Checksum: " << ntohs(ip_header->ip_sum) << std::endl;
+        const struct udphdr* udp_header = (struct udphdr*)(packet + ETHERNET_HEADER_LEN + ip_header_len);
 
-        // 輸出源地址和目的地址
-        struct in_addr src_addr = ip_header->ip_src;
-        struct in_addr dst_addr = ip_header->ip_dst;
-        std::cout << "  Source IP: " << inet_ntoa(src_addr) << std::endl;
-        std::cout << "  Destination IP: " << inet_ntoa(dst_addr) << std::endl;
-        std::cout << "------------------------------" << std::endl;
+        std::string dst_ip = inet_ntoa(ip_header->ip_dst);
+        uint16_t dst_port = ntohs(udp_header->uh_dport);
+        auto dst_ip_port = dst_ip = dst_ip + "," + std::to_string(dst_port);
 
-        char dest_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(ip_header->ip_dst), dest_ip, INET_ADDRSTRLEN);
-
-        ip_count_map[dest_ip]++;
+        ip_port_count[dst_ip_port]++;
     }
-
-    for (const auto &entry : ip_count_map) {
-        std::cout << "Destination IP: " << entry.first << " -> Packet Count: " << entry.second << std::endl;
-    }
-
     pcap_close(handle);
+
+    std::ofstream csv_file("group-by-ip-port.csv");
+    if (!csv_file.is_open()) {
+        std::cerr << "Error opening output file." << std::endl;
+        return;
+    }
+
+    std::cout << "IP,Port,Count" << std::endl;
+    csv_file << "IP,Port,Count" << std::endl;
+    for (const auto& entry : ip_port_count) {
+        std::cout << entry.first << "," << entry.second << std::endl;
+        csv_file << entry.first << "," << entry.second << std::endl;
+    }
+
+    csv_file.close();
+}
+
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        std::cerr << "Usage: ./main group-by-ip-port <filename>.pcap or ./main gap-stats <filename1>.pcap <filename2>.pcap" << std::endl;
+        return 1;
+    }
+
+    auto cmd = std::string(argv[1]);
+    if(cmd == "group-by-ip-port") {
+        auto filepath = argv[2];
+        GroupByIPPort(filepath);
+    }
+    else if(cmd == "gap-stats") {
+        std::cerr << "CMD gap-stats haven't implemented yet.";
+    }
+    else {
+        std::cerr << "Usage: ./main group-by-ip-port <filename>.pcap or ./main gap-stats <filename1>.pcap <filename2>.pcap" << std::endl;
+        return 1;
+    }
+    
     return 0;
 }
